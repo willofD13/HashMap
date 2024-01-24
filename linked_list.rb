c6c83ba7393d21b95c86a96a1d23355346a9af30
willofD13 <leonidastsiakas13@gmail.com>
2024-01-24 17:56:48 +0200

    Add append method to linked list

--- linked_list.rb
+++ linked_list.rb
@@ -15,4 +15,14 @@ class LinkedList
     def initialize(key,value)
         @head = Node.new(key,value) 
     end
+
+    def append(value)
+        node_to_add = head 
+
+        while node_to_add.next !=nil 
+            node_to_add = node_to_add.next 
+        end 
+
+        node_to_add.next = Node.new(value)
+    end
 end
