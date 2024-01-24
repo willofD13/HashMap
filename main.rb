569747aff7193dbc2bf267a2d1c0b6e1282940b4
willofD13 <leonidastsiakas13@gmail.com>
2024-01-24 15:58:38 +0200

    Complete the hash function

--- main.rb
+++ main.rb
@@ -1,4 +1,10 @@
 class HashMap 
+    attr_accessor :hash_table
+
+    def initialize
+        @hash_table = Array.new(16)
+    end
+
     def hash(string)
         hash_code = 0 
         prime_num = 31
@@ -6,6 +12,8 @@ class HashMap
         string.each_char { |char| hash_code = hash_code*prime_num + char.ord }
 
         hash_code
+
+        index = hash_code % @hash_table.size
+        index
     end
 end 
-
